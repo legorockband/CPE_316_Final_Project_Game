99919d662fa29f4f2e917f5b0fe1ec61fdbb14c0
legorockband <115738168+legorockband@users.noreply.github.com>
2025-03-11 12:26:25 -0700

    Created a shitty lock

--- Source/Level.c
+++ Source/Level.c
@@ -11,6 +11,9 @@
 #include "Level.h"
 #include "Terminal.h"
 
+uint8_t x_lock = 40;
+uint8_t y_lock = 40;
+
 char boarder_char[2] = {0XA9, 0};
 
 char key_ascii[3][2] =
@@ -18,6 +21,16 @@ char key_ascii[3][2] =
 		{0x4F, 0},		// O, Key Handle
 		{0x2D, 0},		// -
 		{0x7C, 0}		// |
+};
+
+char lock_ascii[6][2]=
+{
+		{0x5F, 0},		// "_"
+		{0x2F, 0},		// "/"
+		{0x5C, 0},		// "\"
+		{0x28, 0},		// "("
+		{0x29, 0},		// ")"
+		{0x7C, 0}		// "|"
 };
 
 extern UART_HandleTypeDef huart2;
@@ -72,12 +85,19 @@ void boarder(void){
 	}
 }
 
+void title(void){
+
+	//TODO: Create a title for the top of the screen using the ASCII text generator
+
+
+
+}
 
 void stage1(void){
 	boarder();
 	key();
+	lock();
 }
-
 
 void key(void){
 	/*
@@ -95,8 +115,46 @@ void key(void){
 	goto_send(x+1, y+1, key_ascii[2]);	// Create second tooth
 }
 
-void door(void){
+void lock(void){
+	/*
+	   __
+	 _/__\_
+	 | () |
+	 | /\ |
+	 |____|
+	 */
 
+	// Top line
+	goto_send(x_lock, y_lock-2, lock_ascii[0]);		// Top (left) of handle "_"
+	goto_send(x_lock+1, y_lock-2, lock_ascii[0]);	// Top (right) of handle "_"
+
+	// Second line
+	goto_send(x_lock-2, y_lock-1, lock_ascii[0]);	// Top right corner "_"
+	goto_send(x_lock-1, y_lock-1, lock_ascii[1]);	// "/" for lock handle
+	goto_send(x_lock, y_lock-1, lock_ascii[0]);		// Above the "(", "_"
+	goto_send(x_lock+1, y_lock-1, lock_ascii[0]);	// Above the ")", "_"
+	goto_send(x_lock+2, y_lock-1, lock_ascii[2]);	// "\" for lock handle
+	goto_send(x_lock+3, y_lock-1, lock_ascii[0]);	// Top left corner "_"
+
+	// Third Line
+	goto_send(x_lock-2, y_lock, lock_ascii[5]); 	// Left of Center "|"
+	goto_send(x_lock, y_lock, lock_ascii[3]); 		// Center of the lock "("
+	goto_send(x_lock+1, y_lock, lock_ascii[4]);		// The other ")"
+	goto_send(x_lock+3, y_lock, lock_ascii[5]);		// Right edge from center "|"
+
+	// Fourth Line
+	goto_send(x_lock-2, y_lock+1, lock_ascii[5]);	// Left edge "|"
+	goto_send(x_lock, y_lock+1, lock_ascii[1]);		// Under the "(", "/"
+	goto_send(x_lock+1, y_lock+1, lock_ascii[2]);	// Under the ")", "\"
+	goto_send(x_lock+3, y_lock+1, lock_ascii[5]);	// Right edge "|"
+
+	// Last Line
+	goto_send(x_lock-2, y_lock+2, lock_ascii[5]);	// Bottom Left "|"
+	goto_send(x_lock-1, y_lock+2, lock_ascii[0]);	// Bottom "_"
+	goto_send(x_lock, y_lock+2, lock_ascii[0]);		// Bottom "_"
+	goto_send(x_lock+1, y_lock+2, lock_ascii[0]);	// Bottom "_"
+	goto_send(x_lock+2, y_lock+2, lock_ascii[0]);	// Bottom "_"
+	goto_send(x_lock+3, y_lock+2, lock_ascii[5]);	// Bottom Right "|"
 
 
 }
